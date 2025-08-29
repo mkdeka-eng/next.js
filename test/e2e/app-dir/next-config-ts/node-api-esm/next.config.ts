11f7bd2afb985c59b27c2098322d9af62b4ff45f
devjiwonchoi <devjiwonchoi@gmail.com>
2025-08-29 03:30:32 +0200

    Revert "test: need allowImportingTsExtensions"
    This reverts commit 404960c1a1cfea3d3669ce48773ba4a8b54bfea7.

--- test/e2e/app-dir/next-config-ts/node-api-esm/next.config.ts
+++ test/e2e/app-dir/next-config-ts/node-api-esm/next.config.ts
@@ -3,7 +3,6 @@ import fs from 'node:fs'
 import { dirname, join } from 'node:path'
 import { fileURLToPath } from 'node:url'
 
-// import.meta.url only works in ESM
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
